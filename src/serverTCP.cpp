b518d2893a8323c9c87c16a0d85d1a15c8cadcd8
matteo <matteo.massimello@gmail.com>
2024-11-28 14:38:27 +0100

    feat: control the method and for now know the get method

--- src/serverTCP.cpp
+++ src/serverTCP.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -10,13 +11,10 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
-int takeFromHead(std::string buffer, int start) {
+std::size_t takeFromHead(std::string buffer, size_t start) {
 
-    int position = start;
+    std::size_t position = buffer.find(' ', start);
 
-    while(buffer[position] != ' ' || buffer[position] != '\n') {
-        position++;
-    }
 
     return position;
 }
@@ -49,28 +47,30 @@ int main() {
         int client = accept(socketServer, nullptr, nullptr);
 
         char buffer[1024] = {0};
-        recv(client, buffer, sizeof(buffer), 0);
+        ssize_t byte_buffer = recv(client, buffer, sizeof(buffer), 0);
 
         if(strcmp(buffer, ":q") == 0) {
             close(socketServer);
             break;
         }
 
-        std::string sendBack = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
-        send(client, sendBack.c_str(), sendBack.length(), 0);
+        std::string request(buffer, byte_buffer);
 
-        //int method = takeFromHead(buffer, 0);
-        //int where = takeFromHead(buffer, method);
-        //int prot = takeFromHead(buffer, where);
+        std::size_t fmeth = takeFromHead(request, 0);
+        std::string method = request.substr(0, fmeth);
+        std::size_t where = takeFromHead(request, fmeth+1);
+        std::string path = request.substr(fmeth+1, where - fmeth - 1);
 
-        //std::cout<<"here"<<std::endl;
-        //std::string meth = takeTheString(buffer, 0, method);
-        //std::string path = takeTheString(buffer, method, where);
-        //std::string protocol = takeTheString(buffer, where, prot);
+        std::cout << path << std::endl;
 
-        //if (meth.compare("GET") == 0 && path.compare("/") == 0 && protocol.compare("HTTP/1.1") == 0) {
+         if (method == "GET"){
+            std::string sendBack = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+            send(client, sendBack.c_str(), sendBack.length(), 0);
+        } else {
+            std::string sendBack = "HTTP/1.1 501 Not Implemented\r\nContent-Type: text/html\r\n\r\n<html><body><h1>501 Not Implemented</h1></body></html>";
+            send(client, sendBack.c_str(), sendBack.length(), 0);
+        }
 
-        //}
     }
 
 
